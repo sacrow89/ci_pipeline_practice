482aaea91b79cd37299d3f1223304593c0252fbe
sacrow89 <136463411+sacrow89@users.noreply.github.com>
2026-05-24 00:08:00 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
@@ -9,5 +9,3 @@ def test_subtract():
 def test_multiply():
     assert multiply(3, 4) == 12
 
-def test_divide():
-    assert divide(10, 2) == 5
