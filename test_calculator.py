bbbea2537befeb62daef58a3eee79b893241cfcd
sacrow89 <136463411+sacrow89@users.noreply.github.com>
2026-05-24 00:12:21 +0100

    Bugfix/divide (#2)
    * Update calculator.py
    
    * Update test_calculator.py
    
    * Update calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract, multiply
+from calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
@@ -9,3 +9,5 @@ def test_subtract():
 def test_multiply():
     assert multiply(3, 4) == 12
 
+def test_divide():
+    assert divide(10, 2) == 5
